02359aa44e6efc648c28915c7138d259c8596ee6
Sumedh_kamble <136175353+Sumedhk005@users.noreply.github.com>
2024-02-12 17:57:51 +0530

    Add files via upload

---
+++ project/components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>123456789</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
+         <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope"></i><span>shaikhanas@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>mumbai, india - 400104</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
